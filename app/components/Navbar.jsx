8a6ea7f40eef687dd9eed608af9704b2e0589962
Raymond <njagala.raymond@gmail.com>
2024-01-09 20:51:01 +0300

    Added CSS styles to the layout page, deleted some content from the page, added a logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
 import React from 'react'
+import Logo from './dojo-logo.png'
 
 function Navbar(){
   return (
     <nav>
+        <Image
+        src={Logo}
+        alt='Dojo Helpdesk logo'
+        width={70}
+        quality={100}
+        placeholder='blur'
+        />
           <h1>Dojo Help desk</h1>
           <Link href="/">Dashboard</Link>
           <Link href="/tickets">Tickets</Link>
